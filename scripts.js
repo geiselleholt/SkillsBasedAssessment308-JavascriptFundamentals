df3d386870dd03fc6b9b5bcb2942e53af2663469
Geiselle Holt <geiselleholt@hotmail.com>
2025-03-21 12:01:57 -0600

    Added in instructions and started structuring the result

--- scripts.js
+++ scripts.js
@@ -1,8 +1,52 @@
-function getLearnerData(course, ag, submissions) {
-  // here, we would process this data to achieve the desired result.
+//INSTRUCTIONS:
+// Your goal is to analyze and transform this data such that the output of your program is an array of objects, each containing the following information in the following format:
+// {
+//     // the ID of the learner for which this data has been collected
+//     "id": number,
 
-  return result;
+//     // the learner’s total, weighted average, in which assignments
+//     // with more points_possible should be counted for more
+//     // e.g. a learner with 50/100 on one assignment and 190/200 on another
+//     // would have a weighted average score of 240/300 = 80%.
+//     "avg": number,
+
+//     // each assignment should have a key with its ID,
+//     // and the value associated with it should be the percentage that
+//     // the learner scored on the assignment (submission.score / points_possible)
+//     <assignment_id>: number,
+
+//     // if an assignment is not yet due, it should not be included in either
+//     // the average or the keyed dictionary of scores
+// }
+
+// Afterwards, alter the data to test for edge cases, error handling, and other potential issues.
+// If an AssignmentGroup does not belong to its course (mismatching course_id), your program should throw an error, letting the user know that the input was invalid. Similar data validation should occur elsewhere within the program.
+// You should also account for potential errors in the data that your program receives. What if points_possible is 0? You cannot divide by zero. What if a value that you are expecting to be a number is instead a string?
+// Use try/catch and other logic to handle these types of errors gracefully.
+// If an assignment is not yet due, do not include it in the results or the average. Additionally, if the learner’s submission is late (submitted_at is past due_at), deduct 10 percent of the total points possible from their score for that assignment.
+
+
+let expectedOutput = []
+let learnerAnalysis = {
+    id: '',
+    avg: '',
 }
+let learners = []
+function getLearnerData(course, ag, submissions) {
+    submissions.forEach(submission => {
+    if (learners.includes(submission.learner_id) == false) {
+        learnerAnalysis.id = submission.learner_id
+        learners.push(submission.learner_id)
+    } else {
+        console.log("ok")
+    }
+});
+console.log(learnerAnalysis)
+
+//   return result;
+}
+
+
 
 // The provided course information.
 const CourseInfo = {
